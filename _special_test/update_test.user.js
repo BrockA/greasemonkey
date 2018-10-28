dec8acaac9db2630ccea656ee3b49dd075c222f0
Brock <659011+BrockA@users.noreply.github.com>
2018-10-28 10:46:11 -0700

    Create update_test.user.js

---
+++ _special_test/update_test.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name     _Update on name change test
+// @match    http://mailfocus.net/switch_board_x.htm
+// @version  0.1
+// @grant    none
+// ==/UserScript==
+
+// @grant mode has no effect on this.
+console.log ("GM_info.script.name: ", GM_info.script.name);
+console.log ("GM_info.script.version: ", GM_info.script.version);
+console.log ("GM_info: ", GM_info);
